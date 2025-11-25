8879d7e73366c3fb0965aea9274bea41685c988f
Goutham <gouthamshastry02@gmail.com>
2025-11-25 08:40:49 +0530

    clean

--- src/App.jsx
+++ src/App.jsx
@@ -152,34 +152,6 @@ db.category_sales.find().pretty()
                 `
             } />
 
-            <One topic={"4"} text={
-                `
-import pandas as pd
-def find_s_algorithm(file_path):
-    data = pd.read_csv(file_path)
-    print("Training data:")
-    print(data)
-
-    attributes = data.columns[:-1]
-    class_label = data.columns[-1]
-
-    hypothesis = ['?' for _ in attributes]
-
-    for index, row in data.iterrows():
-        if row[class_label] == 'Yes': 
-            for i, value in enumerate(row[attributes]):
-                if hypothesis[i] == '?' or hypothesis[i] == value:
-                    hypothesis[i] = value
-                else:
-                    hypothesis[i] = '?'
-
-    return hypothesis
-file_path = ''
-final_hypothesis = find_s_algorithm(file_path)
-print("\\\\nThe final hypothesis is:", final_hypothesis)
-               `
-            } />
-
             <One topic={"mr_calc"} text={
                 `
 Mapper Program — mapper.py 
